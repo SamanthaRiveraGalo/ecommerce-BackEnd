cda1345f892e8768c3b71e03bd01607a575ba2ab
Samantha Rivera Galo <samirivera1808@gmail.com>
2024-04-03 23:22:54 -0300

    testing final

--- tests/products.test.js
+++ tests/products.test.js
@@ -5,27 +5,38 @@ const requester = supertest('http://localhost:8080')
 
 describe('Testing de products', () => {
 
-    // it('should return a list of products', async () => {
+    it('should return a list of products', async () => {
 
-    //     const response = await requester.get('/api/products');
+        const response = await requester.get('/api/products')
 
-    //     expect(response.status).to.equal(200);
-    //     expect(response._body.payload.payload).to.be.an('array');
-    // })
+        expect(response.status).to.equal(200)
+        expect(response._body.payload.payload).to.be.an('array')
+    })
 
-    // it('Deberia traer un producto por el id', async () => {
+    it('Deberia traer un producto por el id', async () => {
 
-    //     const productId = '658383513b801dcfe9bc2075'
+        const productId = '658383513b801dcfe9bc2075'
 
-    //     const response = await requester.get(`/api/products/${productId}`)
-    //     const { statusCode, ok } = response
+        const response = await requester.get(`/api/products/${productId}`)
+        const { statusCode, ok } = response
 
-    //     expect(statusCode, 200)
-    //     expect(ok, true)
-    //     expect(response._body.payload).to.have.property('_id')
-    // })
+        expect(statusCode, 200)
+        expect(ok, true)
+        expect(response._body.payload).to.have.property('_id')
+    })
 
     it('Deberia agregar un nuevo producto', async () => {
+
+        let cookieHeader
+
+        const userMock = {
+            email: "adminCoder@coder.com",
+            password: "1234",
+        }
+
+        const response = await requester.post(`/api/login`).send(userMock)
+
+        cookieHeader = response.header['set-cookie'][0]
 
         const newProduct = {
             title: "Nuevo Producto",
@@ -34,16 +45,15 @@ describe('Testing de products', () => {
             price: 800,
             stock: 10,
             thumbnail: "sin img",
-            category: "mujer"
+            category: "mujer",
         }
 
-        const response = await requester.post('/api/products').send(newProduct).set()
+        const responseProduct = await requester.post('/api/products').send(newProduct).set('Cookie', cookieHeader)
 
-        console.log(response._body)
+        const { statusCode } = responseProduct
 
-        // expect(response).to.have.status(200)
-        expect(response.body.status).to.equal('success')
-        // expect(response._body.payload).to.have.property('_id')
+        expect(statusCode, 200)
+
     })
 
 })
--- tests/session.test.js
+++ tests/session.test.js
@@ -5,48 +5,49 @@ const requester = supertest('http://localhost:8080')
 
 describe('Test para sessions', () => {
     let cookie
+    let cookieResult
 
-    // it('Deberia registrar un usuario', async () => {
-    //     let userMock = {
-    //         first_name: 'sam',
-    //         last_name: 'prueba testing',
-    //         email: 'sam@gmail.com',
-    //         password: '12345'
-    //     }
+    it('Deberia registrar un usuario', async () => {
+        let userMock = {
+            first_name: 'sam',
+            last_name: 'prueba testing',
+            email: 'sam@gmail.com',
+            password: '12345'
+        }
 
-    //     const response = await requester.post('/api/sessions/register').send(userMock)
+        const response = await requester.post('/api/sessions/register').send(userMock)
+        expect(response).to.be.ok
+    })
 
-    //     expect(response).to.be.ok
-    // })
+    it('Deberia ingresar un usuario correctamente y devolver una cookie', async () => {
 
-    // it('Deberia ingresar un usuario correctamente y devolver una cookie', async () => {
+        const userMock = {
+            email: "adminCoder@coder.com",
+            password: "1234",
+        }
 
-    //     const userMock = {
-    //         email: "s@gmail.com",
-    //         password: "123456",
-    //     }
+        const response = await requester.post(`/api/login`).send(userMock)
 
-    //     const response = await requester.post(`/api/login`).send(userMock)
+        cookieResult = response.header['set-cookie'][0]
 
-    //     const cookieResult = response.header['set-cookie'][0]
+        expect(cookieResult).to.be.ok
 
-    //     expect(cookieResult).to.be.ok
+        cookie = {
+            name: cookieResult.split('=')[0],
+            value: cookieResult.split('=')[1]
+        }
 
-    //     cookie = {
-    //         name: cookieResult.split('=')[0],
-    //         value: cookieResult.split('=')[1]
-    //     }
+        expect(cookie.name).to.be.ok.and.eql('connect.sid')
+        expect(cookie.value).to.be.ok
 
-    //     expect(cookie.name).to.be.ok.and.eql('connect.sid')
-    //     expect(cookie.value).to.be.ok
+    })
+
+    it('test para la ruta current', async () => {
         
-    // })
+        const response = await requester.get('/api/sessions/current').set('Cookie', cookieResult)
 
-    it('Deberia cerrar sesion', async () => {
+        const { statusCode } = response
 
-        const response = await requester.post('/api/sessions/logout')
-
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('status', 'success')
+        expect(statusCode, 200)
     })
 })
