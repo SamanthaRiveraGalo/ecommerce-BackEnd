7cb25c2a346f2c70c420327f4cba1c95249af5cf
Samantha Rivera Galo <samirivera1808@gmail.com>
2024-04-17 23:31:43 -0300

    multer.js

--- src/middlewars/multer.js
+++ src/middlewars/multer.js
@@ -1,27 +1,44 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+
+const profilePath = path.resolve(__dirname, "../public/uploads/profile")
+const productsPath = path.resolve(__dirname, "../public/uploads/products")
+const documentsPath = path.resolve(__dirname, "../public/uploads/documents")
 
 const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-    
-        if (file.fieldname === 'imageProfile') {
-            cb(null, path.join(`${__dirname}/public/uploads/profile`))
+    destination: function (req, file, cb) {
+        if (file.fieldname === "imageProfile") {
+            cb(null, profilePath);
         }
 
-        if (file.fieldname === 'imageProduct') {
-            cb(null, path.join(`${__dirname}/public/uploads/products`))
+        if (file.fieldname === "imageProduct") {
+            cb(null, productsPath);
         }
 
-        if (file.fieldname === 'profileDocuments') {
-            cb(null, path.join(`${__dirname}/public/uploads/documents`))
+        if (file.fieldname === "profileDocuments") {
+            cb(null, documentsPath);
         }
     },
 
     filename: function (req, file, cb) {
-        cb (null, `${Date.now()} - ${file.originalname}`)
-    }
+        cb(null, `${Date.now()} - ${file.originalname}`)
+    },
 })
 
-const uploader = multer(storage)
+const createMulterFolders = (folderPath) => {
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true })
+        console.log(`Directorio creado en ${folderPath}`)
+    } else {
+        console.log(`El directorio ${folderPath} ya existe.`)
+    }
+};
 
-module.exports = uploader
+createMulterFolders(profilePath)
+createMulterFolders(productsPath)
+createMulterFolders(documentsPath)
+
+const uploader = multer({ storage })
+
+module.exports = uploader;
